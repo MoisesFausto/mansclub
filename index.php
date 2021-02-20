44a77d0fa23230633f27efbd8a20b24112cc8d1d
Moisés Fausto <moises_system@yahoo.com.br>
2021-02-20 15:03:19 -0300

    Criação da página cadastre-se

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Man's Club</title>
+  <title>Login | Man's Club</title>
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -17,7 +17,7 @@
 </head>
 <body>
 
-  <div class="main_login">
+  <main class="main_login">
     <div class="container-fluid">
       <div class="row">
         <div class="col-6 login_background bg-my-primary d-none d-md-block">
@@ -51,7 +51,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </main>
 
   <!-- scripts -->
   <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
